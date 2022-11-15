bfdb2af49eb10adac34ba9c0efdd556931ba0779
hjun0917 <hjun0917@naver.com>
2022-11-15 18:33:12 +0900

    회원가입 서버 기능 구현(1차)

--- app.py
+++ app.py
@@ -1,27 +1,74 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for, redirect
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://ID:PW@clusterName.spvewgv.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.mapsk1p.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.test
+
+import hashlib
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.html')
 
-@app.route("/sample", methods=["POST"])
-def sample_post():
+
+@app.route('/signup')
+def sign_up():
+    return render_template('signup.html')
+
+
+@app.route('/api/checkid', methods=['POST'])
+def check_id():
+    c_id = 0
+    id_receive = request.form['id_give']
+    users = list(db.user.find({}, {'_id': False}))
+    for user in users:
+        user_id = user['id']
+        if user_id == id_receive:
+            c_id = 1
+    return jsonify({'result': 'success', 'status': c_id})
+
+
+@app.route('/api/checkname', methods=['POST'])
+def check_name():
+    c_name = 0
     name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-    doc = {'name': name_receive, 'comment': comment_receive}
-    db.fancomments.insert_one(doc)
+    users = list(db.user.find({}, {'_id': False}))
+    for user in users:
+        user_name = user['user_name']
+        if user_name == name_receive:
+            c_name = 1
+    return jsonify({'result': 'success', 'status': c_name})
 
-    return jsonify({'msg': '작성 완료!'})
 
-@app.route("/sample", methods=["GET"])
-def sample_get():
-    cheer_comments = list(db.fancomments.find({}, {'_id': False}))
-    return jsonify(cheer_comments)
+@app.route('/api/checkmail', methods=['POST'])
+def check_mail():
+    c_mail = 0
+    mail_receive = request.form['mail_give']
+    users = list(db.user.find({}, {'_id': False}))
+    for user in users:
+        user_mail = user['mail']
+        if user_mail == mail_receive:
+            c_mail = 1
+    return jsonify({'result': 'success', 'status': c_mail})
+
+
+@app.route('/api/signup', methods=['POST'])
+def join():
+    id_receive = request.form['id_give']
+    name_receive = request.form['name_give']
+    mail_receive = request.form['mail_give']
+    pw_receive = request.form['pw_give']
+
+    # 패스워드 단방향 암호화
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.user.insert_one({'id': id_receive, 'user_name': name_receive, 'mail': mail_receive, 'pw': pw_hash})
+    return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=3000, debug=True)
